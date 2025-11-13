99d5e4c263b75847981b470627c7488afa541fc4
DaviChalita <chalitameneguelli14@gmail.com>
2025-11-13 20:08:59 -0300

    validate input for ordering

---
+++ runewell/list/commons/Enums.py
@@ -0,0 +1,10 @@
+from enum import StrEnum
+
+
+class OrderBy(StrEnum):
+    NAME = 'name'
+    COST = 'cost'
+    TYPE = 'type'
+    MIGHT = 'might'
+    SET_NAME = 'set_name'
+    RARITY = 'rarity'
--- runewell/list/views.py
+++ runewell/list/views.py
@@ -1,15 +1,17 @@
-import json
-
 from django.core.paginator import Paginator
 from django.shortcuts import render
 
+from .commons.Enums import OrderBy
 from .models import Card
 
 
-# Create your views here.
-
 def list(request):
-    card_list = Card.objects.all()
+    order_request = request.GET.get("order")
+    if order_request not in [order_by.value for order_by in OrderBy]:
+        order_request = None
+    order = 'name' if order_request is None else order_request
+    direction = '' if request.GET.get("dir") is None or request.GET.get("dir") == 'asc' else '-'
+    card_list = Card.objects.all().order_by(f"{direction}{order}")
     paginator = Paginator(card_list, 60)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
