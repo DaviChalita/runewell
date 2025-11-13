6ba57b23bae9c5a388528919753f3d0aaa692fa6
DaviChalita <chalitameneguelli14@gmail.com>
2025-11-12 23:46:37 -0300

    att to database postgres + modeling + venvs

--- runewell/list/models.py
+++ runewell/list/models.py
@@ -1,14 +1,23 @@
+from django.contrib.postgres.fields import ArrayField
 from django.db import models
 from django.views.generic import ListView
 
+class Card(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.TextField()
+    effect = models.TextField()
+    color = ArrayField(models.TextField(null=True,blank=True), blank=True,)
+    cost = models.TextField()
+    type = models.TextField()
+    might = models.TextField()
+    tags = ArrayField(models.TextField(null=True,blank=True), blank=True,)
+    set_name = models.TextField()
+    rarity = models.TextField()
+    image = models.TextField()
 
-# Create your models here.
+    class Meta:
+        db_table = 'cards'
 
-class Card:
-    image: str
-
-    def __init__(self,image):
-        self.image = image
 
 class CardListView(ListView):
     paginated_by = 2
