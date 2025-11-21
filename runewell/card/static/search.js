c397743b8c2752fc37be2c608eb7d8cedc2fa095
DaviChalita <chalitameneguelli14@gmail.com>
2025-11-20 23:42:31 -0300

    update search list

---
+++ runewell/card/static/search.js
@@ -0,0 +1,36 @@
+function pageNavigate(page){
+    var searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("page", page);
+    window.location.search = searchParams.toString();
+}
+
+function updateSortOrder(e){
+    const sortOrder = e.target.value;
+    var searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("order", sortOrder);
+    window.location.search = searchParams.toString();
+}
+
+function updateSortDir(e){
+    const sortDir = e.target.value;
+    var searchParams = new URLSearchParams(window.location.search);
+    searchParams.set("dir", sortDir);
+    window.location.search = searchParams.toString();
+}
+
+function setUpSelectorChangeHandler(id, searchParamName, changeHandler){
+    const sortSelector = document.getElementById(id);
+    const param = new URLSearchParams(location.search);
+    const searchParamValue = param.get(searchParamName);
+    sortSelector.value = searchParamValue;
+    sortSelector.addEventListener("change", changeHandler);
+}
+
+function startUp(){
+    setUpSelectorChangeHandler("order_id_header", "order", updateSortOrder);
+    setUpSelectorChangeHandler("dir_id_header", "dir", updateSortDir);
+    setUpSelectorChangeHandler("order_id_footer", "order", updateSortOrder);
+    setUpSelectorChangeHandler("dir_id_footer", "dir", updateSortDir);
+}
+
+window.onload = startUp;
