69b5531dbd5c837307c99ae1901d79f73211b761
DaviChalita <chalitameneguelli14@gmail.com>
2025-12-11 14:52:13 -0300

    wip

--- runewell/card/urls.py
+++ runewell/card/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import list, details, advanced_search_form
+from .views import list, details, advanced_search
 
 urlpatterns = [
     path("card/list", list.search_list, name="list"),
     path("card/<int:card_id>/", details.card_details),
+    path("card/advanced", advanced_search.advanced_search_form),
 ]
--- runewell/card/views/advanced_search_form.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-
-def advanced_search_form():
-    return render("advanced_search/advanced_search.html")
