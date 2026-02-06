cf7585e97ca7c5308e2a73fb79fabd0908d44d6e
DaviChalita <chalitameneguelli14@gmail.com>
2026-02-06 18:06:59 -0300

    wip

--- runewell/card/urls.py
+++ runewell/card/urls.py
@@ -5,5 +5,5 @@ from .views import list, details, advanced_search
 urlpatterns = [
     path("card/list", list.search_list, name="list"),
     path("card/<int:card_id>/", details.card_details),
-    path("card/advanced", advanced_search.advanced_search_form),
+    path("card/advanced", advanced_search.advanced_search_form,name="card_advanced"),
 ]
