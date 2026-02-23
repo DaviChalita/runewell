fedbd188f705c7df2955b14ddb97139994ef3843
DaviChalita <chalitameneguelli14@gmail.com>
2026-02-23 13:14:30 -0300

    add venv to work both prod and dev server

--- runewell/runewell/settings.py
+++ runewell/runewell/settings.py
@@ -35,7 +35,7 @@ MIDDLEWARE = [
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("DEBUG_MODE")
 
 ALLOWED_HOSTS = ['*']
 
