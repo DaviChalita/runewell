6ad419e3c7d8e31ddc89afce896cbe4733698b2e
DaviChalita <chalitameneguelli14@gmail.com>
2026-04-02 18:12:58 -0300

    update rarities add is promo add is token

--- runewell/card/views/advanced_search.py
+++ runewell/card/views/advanced_search.py
@@ -1,7 +1,6 @@
 from django.db.models import Q
 from django.shortcuts import render
 
-from ..commons.rarity_enum import Rarity
 from ..models.card import Card
 
 
@@ -10,9 +9,5 @@ def advanced_search_form(request):
     supertypes = Card.objects.filter(~Q(supertype=None)).values_list('supertype', flat=True).distinct().order_by(
         'supertype')
     sets = Card.objects.values_list('set_name', flat=True).distinct().order_by('set_name')
-    rarities = Card.objects.values_list('rarity', flat=True).distinct().order_by('rarity')
-    rarity_list = []
-    for rarity in rarities:
-        rarity_list.append(Rarity(int(rarity)).name)
     return render(request, "advanced_search/advanced_search.html",
-                  {"types": list(types), "supertypes": list(supertypes), "sets": list(sets), "rarities": rarity_list})
+                  {"types": list(types), "supertypes": list(supertypes), "sets": list(sets)})
